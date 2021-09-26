e972c5607d752784105817432d313eb7c9415fde
GitHub Action <action@github.com>
2021-09-26 00:53:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/charlesowen/wxwidgetsclionplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.charlesowen.wxwidgetsclionplugin.services
+
+import com.github.charlesowen.wxwidgetsclionplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
